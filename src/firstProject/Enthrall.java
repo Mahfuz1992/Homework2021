c07820b44fbdf2188bd585e860a5c3991370aa03
mahfuz.rahman <Mafzz.123@gmail.com>
2021-10-22 18:07:32 -0400

    10\22\2021

---
+++ src/firstProject/Enthrall.java
@@ -0,0 +1,37 @@
+package firstProject;
+
+public class Enthrall {
+
+	
+	public static void main (String[]args) {  
+	System.out.println("Mahfuz Rahman");
+	System.out.println("1920 powell ave");
+	System.out.println("bronx");
+	System.out.println("ny");
+	System.out.println("10462");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
